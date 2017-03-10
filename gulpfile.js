788c6c314edd91195605091b5d425175cd2c6acd
FarhadMohammadi <mailer.laravel.developer@gmail.com>
2017-03-10 17:26:36 +0330

    v0.5.0 - add gulp and elixir to app

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var elixir = require('laravel-elixir');
+elixir.config.assetsPath = '';
+elixir.config.js.folder = '';
+
+elixir(function (mix) {
+    mix.scripts([
+        'resources/assets/angular/user/config/',
+        'resources/assets/angular/user/controllers/',
+        'resources/assets/angular/user/directives/',
+        'resources/assets/angular/user/services/'
+    ], 'public/js');
+});
